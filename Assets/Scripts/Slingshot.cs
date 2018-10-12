d0d25beba9a1c7cb7675c1c1eae8be3a62bb69eb
Zeen172 <Zeen172@gmail.com>
2018-10-11 22:05:31 -0400

    added halo affect

--- Assets/Scripts/Slingshot.cs
+++ Assets/Scripts/Slingshot.cs
@@ -3,11 +3,23 @@ using System.Collections;
 
 public class Slingshot : MonoBehaviour {
 
-    void OnMouseEnter() {
-        print("Slingshot:OnMouseEnter()");
+    public GameObject launchPoint;
+
+    void Awake() {
+        Transform launchPointTrans = transform.Find("LaunchPoint");
+        // a 
+        launchPoint = launchPointTrans.gameObject;
+        launchPoint.SetActive( false );
+        // b 
+    }
+
+        void OnMouseEnter() {
+        //print("Slingshot:OnMouseEnter()");
+        launchPoint.SetActive(true);
     }
 
     void OnMouseExit() {
-        print("Slingshot:OnMouseExit()");
+        //print("Slingshot:OnMouseExit()");
+        launchPoint.SetActive(false);
     }
 }
